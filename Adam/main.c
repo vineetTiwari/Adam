cfe0e90f8c63b3b126fa75bbdef587ac17a1cd37
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-05-05 14:05:12 -0700

    IT WORKSSS

--- Adam/main.c
+++ Adam/main.c
@@ -7,9 +7,57 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+
+
+bool yelling (char *string) {
+
+  for ( char i = 0; i < strlen(string); i++) {
+
+    if (islower(string[i])) {
+
+      return false;
+    }
+    
+  }
+
+  return true;
+}
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
-    return 0;
+  char input[1024];
+
+  while (1) {
+    scanf("%1024[^\n]", input);
+
+    fpurge(stdin);
+
+    if (strcasecmp(input, "Exit") == 0)
+    {
+      break;
+
+    } else if (strcasecmp(input, "Adam") == 0) {
+
+      printf("Fine. Be that way!\n");
+
+    } else if (yelling(input)) {
+
+      printf("wow! chill out.\n");
+
+    } else if (strchr(input, '?')) {
+
+      printf("Sure\n");
+
+    } else {
+
+      printf("Whatever\n");
+    }
+
+
+  }
+
+  return 0;
 }
